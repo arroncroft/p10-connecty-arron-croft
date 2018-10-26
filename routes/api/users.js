e6a732b6a930ec6d7a17ad520f99adfb03fd8852
arroncroft <arroncroft@hotmail.com>
2018-10-26 19:24:56 -0400

    finished project

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 // Load User model
 const User = require('../../models/User');
 
@@ -18,6 +22,11 @@ router.get("/test", (req, res) => res.json({ msg: "Users works." })); // The rou
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({ email: req.body.email }) // asynchronous, therefore must be handled using a promise
     .then(user => {
       if (user) {
@@ -54,6 +63,12 @@ router.post("/register", (req, res) => {
 // @desc    login user and return a JWT token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
