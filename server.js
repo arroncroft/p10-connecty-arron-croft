aece0b4aee21b88adef9bd9c584d49b7298ff7dd
arroncroft <arroncroft@hotmail.com>
2018-10-25 21:43:08 -0400

    fixed untracked files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('hello'));
+
+// The actual port for the app in prodcution will come from the env. var. 
+// "PORT". For local dev., we just use 5000.
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
